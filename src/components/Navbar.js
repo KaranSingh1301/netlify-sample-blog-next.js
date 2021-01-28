14aaecb8188c10e689674818f5f693462f3c474c
karan singh <kssinghkaran13@gmail.com>
2021-01-28 14:55:19 +0530

    aa

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navbar() {
+
+    const mystyle = {
+        color: "white",
+        backgroundColor:"grey",
+        width: "auto",
+        height: "60px",
+        fontFamily: "Arial",
+        paddingTop:"0"
+
+      };
+
+    return (
+        <>
+            <h1 style={mystyle}>Pro Garten</h1>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Progarten sample blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
        <h1>Blog.</h1>
     </div>
   )
